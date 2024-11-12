6ab235dd9c4b3f86930d7370726cf934501ad91b
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2024-11-12 18:50:53 +0000

    rename `homepage` to `dashboard`, create `login` and use cookies based sessions

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, session
+from helpers import login_required
 
 app = Flask(__name__)
+app.secret_key = "ChaveDeTeste"
 
 @app.route("/")
-def homepage():
-    return render_template("homepage.html")
+def index():
+    if session:
+        return redirect("/dashboard")
+    else:
+        return redirect("/login")
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
