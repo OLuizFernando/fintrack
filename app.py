52119430af9140872b11bde476d6b4db86ae7a5c
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2024-11-13 17:44:27 -0300

    add script to connect to postgres database easier

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-from flask import Flask, redirect, render_template, request, session
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, g, redirect, render_template, request, session
 from helpers import login_required
 
+load_dotenv()
+
 app = Flask(__name__)
-app.secret_key = "ChaveDeTeste"
+app.secret_key = os.getenv("SECRET_KEY")
+app.config['DATABASE'] = {
+    'host': os.getenv("PGHOST"),
+    'dbname': os.getenv("PGDATABASE"),
+    'user': os.getenv("PGUSER"),
+    'password': os.getenv("PGPASSWORD")
+}
+
+def get_db():
+    return psycopg2.connect(
+        host=app.config['DATABASE']['host'],
+        dbname=app.config['DATABASE']['dbname'],
+        user=app.config['DATABASE']['user'],
+        password=app.config['DATABASE']['password']
+    )
+
+def db_execute(query, params=None):
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute(query, params)
+    results = cursor.fetchall()
+    db.close()
+    return results
 
 @app.route("/")
 def index():
